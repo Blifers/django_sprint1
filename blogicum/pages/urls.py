e2b526cc584a50a8bf4fbd82fbd0c25c20271d8e
Ilya Panin <pwilya2@yandex.ru>
2024-04-23 23:51:18 +0700

    index.html is ready

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', views.about, name='pages_about'),
-    path('rules/', views.rules, name='pages_rules'),
+    path('about/', views.about, name='about'),
+    path('rules/', views.rules, name='rules'),
 ]
